aa3a258a085ab6bfdcfdff0c2415c4b961d15fad
Drew <hamiltondrewb@gmail.com>
2021-09-24 22:01:15 -0400

    fixes

--- assignment1/client.cpp
+++ assignment1/client.cpp
@@ -18,16 +18,21 @@ using std::endl;
 
 #define MAX_PAYLOAD 16777216
 struct client_arguments {
-	struct sockaddr_in ip_port;
+  struct sockaddr_in ip_port;
 
-	int treq;
-	int timeout;
+  int treq;
+  int timeout;
 };
 
 typedef struct message {
-    uint32_t seq;
-    float theta;
-    float delta;
+  uint32_t seq;
+  float theta;
+  float delta;
+  message(){
+    seq = 0;
+    theta = 0;
+    delta = 0;
+  }
 }message;
 
 
@@ -35,56 +40,56 @@ typedef struct message {
 
 
 error_t client_parser(int key, char *arg, struct argp_state *state) {
-	struct client_arguments *args = (client_arguments *) state->input;
-	error_t ret = 0;
-	switch(key) {
-	case 'a':
-		inet_pton(AF_INET, arg, &(args->ip_port.sin_addr));
-		break;	
-	case 'p':
-		args->ip_port.sin_port = atoi(arg);
-		break;
-	case 'n':
-		/* validate argument makes sense */
-		args->treq = atoi(arg);
-		break;
-	case 't':
-		args->timeout = atoi(arg);
-		break;
-	default:
-		ret = ARGP_ERR_UNKNOWN;
-		break;
-	}
-	return ret;
+  struct client_arguments *args = (client_arguments *) state->input;
+  error_t ret = 0;
+  switch(key) {
+  case 'a':
+    inet_pton(AF_INET, arg, &(args->ip_port.sin_addr));
+    break;	
+  case 'p':
+    args->ip_port.sin_port = atoi(arg);
+    break;
+  case 'n':
+    /* validate argument makes sense */
+    args->treq = atoi(arg);
+    break;
+  case 't':
+    args->timeout = atoi(arg);
+    break;
+  default:
+    ret = ARGP_ERR_UNKNOWN;
+    break;
+  }
+  return ret;
 }
 
 
 client_arguments client_parseopt(int argc, char *argv[]) {
 
-	// memset(args, 0, sizeof(*args));
+  // memset(args, 0, sizeof(*args));
 
-	struct argp_option options[] = {
-		{ "addr", 'a', "addr", 0, "The IP address the server is listening at", 0},
-		{ "port", 'p', "port", 0, "The port that is being used at the server", 0},
-		{ "treq", 'n', "treq", 0, "The number of time requests to send to the server", 0},
-		{ "timeout", 't', "timeout", 0, "The timeout percentage", 0},
-		{0}
-	};
+  struct argp_option options[] = {
+    { "addr", 'a', "addr", 0, "The IP address the server is listening at", 0},
+    { "port", 'p', "port", 0, "The port that is being used at the server", 0},
+    { "treq", 'n', "treq", 0, "The number of time requests to send to the server", 0},
+    { "timeout", 't', "timeout", 0, "The timeout percentage", 0},
+    {0}
+  };
 
-	struct argp argp_settings = { options, client_parser, 0, 0, 0, 0, 0 };
-	struct client_arguments args;
+  struct argp argp_settings = { options, client_parser, 0, 0, 0, 0, 0 };
+  struct client_arguments args;
 
-	bzero(&args, sizeof(args));
+  bzero(&args, sizeof(args));
 
-	if (argp_parse(&argp_settings, argc, argv, 0, NULL, &args) != 0) {
-		printf("Got error in parse\n");
-	}
-	/* If they don't pass in all required settings, you should detect
-	 * this and return a non-zero value from main */
+  if (argp_parse(&argp_settings, argc, argv, 0, NULL, &args) != 0) {
+    printf("Got error in parse\n");
+  }
+  /* If they don't pass in all required settings, you should detect
+   * this and return a non-zero value from main */
 
-	printf("Got port %d with n=%d timeout=%d \n", args.ip_port.sin_port, args.treq, args.timeout);
+  printf("Got port %d with n=%d timeout=%d \n", args.ip_port.sin_port, args.treq, args.timeout);
 
-	return args;
+  return args;
 		   
 	
 
@@ -95,104 +100,105 @@ client_arguments client_parseopt(int argc, char *argv[]) {
 int main(int argc, char *argv[])
 {
 
-	client_arguments client = client_parseopt(argc, argv);
-	int sock = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in serv_addr = client.ip_port;
-	if (sock < 0)
+  client_arguments client = client_parseopt(argc, argv);
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in serv_addr = client.ip_port;
+  if (sock < 0)
     {
-        perror("socket() failed");
+      perror("socket() failed");
     }else{
-		printf("Socket success");
-	}
+    printf("Socket success");
+  }
 
-	memset(&serv_addr, 0, sizeof(serv_addr)); // Zero out structure
-	serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(client.ip_port.sin_port);
+  memset(&serv_addr, 0, sizeof(serv_addr)); // Zero out structure
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(client.ip_port.sin_port);
 
-	 if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
-        perror("\nConnection Failed \n");
+      perror("\nConnection Failed \n");
 		
     }
 
     
-	uint32_t timeout = htonl(client.timeout);
-    uint32_t treq = client.treq;
+  uint32_t timeout = htonl(client.timeout);
+  uint32_t treq = client.treq;
 
-	int i=0;
+  int i=0;
 	
-	while(i< (int)treq)
-	{	
-		//get sequence to be sent
-		uint32_t seq = i +1;
-		seq = htonl(seq);
+  while(i < (int)treq)
+    {	
+      //get sequence to be sent
+      uint32_t seq = i +1;
+      seq = htonl(seq);
 		
-		//version already initialized, doesnt change but needs to be convertion
-		uint16_t ver = 7;
-		ver = htonl(ver);
+      //version already initialized, doesnt change but needs to be convertion
+      uint16_t ver = 7;
+      ver = htonl(ver);
 
-		//get the time stamp to be sent
-        struct timespec curr_t;
-		clock_gettime(CLOCK_REALTIME,&curr_t);
+      //get the time stamp to be sent
+      struct timespec curr_t;
+      clock_gettime(CLOCK_REALTIME,&curr_t);
 
-		time_t sec = curr_t.tv_sec;
-		long nsec = curr_t.tv_nsec;
+      time_t sec = curr_t.tv_sec;
+      long nsec = curr_t.tv_nsec;
 
-		uint8_t data[22]; //data to be sent seq , version, seconds, nano seconds,(8 + 8 + 2 + 4)
+      uint8_t data[22]; //data to be sent seq , version, seconds, nano seconds,(8 + 8 + 2 + 4)
 
-		memcpy(data,&seq, 4);
-		memcpy(data,&seq, 2);
-		memcpy(data,&seq, 8);
-		memcpy(data,&seq, 8);
+      memcpy(data,&seq, 4);
+      memcpy(data,&seq, 2);
+      memcpy(data,&seq, 8);
+      memcpy(data,&seq, 8);
 
-        sendto(sock, data,22, 0, (const struct sockaddr *) &serv_addr, sizeof(serv_addr));
-		i++;
-	}
-
-
+      sendto(sock, data,22, 0, (const struct sockaddr *) &serv_addr, sizeof(serv_addr));
+      i++;
+    }
 
 
-	//recieve from server
 
-	//create poll
-    struct pollfd check[1];
-    check[0].fd = sock;
-    check[0].events = POLLIN;
 
-    struct sockaddr server_addr; // Source address of server
-    socklen_t len = sizeof(server_addr);
-    i = 0;
-    int t_out, c_tout = client.timeout;
+  //recieve from server
 
-    message finish[client.treq];
-    memset(finish, 0, client.treq*sizeof(finish));
+  //create poll
+  struct pollfd check[1];
+  check[0].fd = sock;
+  check[0].events = POLLIN;
 
-    while(i< (int)treq)
+  struct sockaddr server_addr; // Source address of server
+  socklen_t len = sizeof(server_addr);
+  i = 0;
+  int t_out, c_tout = client.timeout;
+
+  message finish[client.treq];
+  //memset(finish, 0, client.treq*sizeof(finish));
+
+  while(i < treq)
     {   
-        // printf("Enters the while loop: i: %d treq: %d\n", i, (int)treq);
-        if(c_tout == 0)
+      // printf("Enters the while loop: i: %d treq: %d\n", i, (int)treq);
+      if(c_tout == 0)
         {
-			printf("time = 0");
-            t_out = -1;
+	  printf("time = 0");
+	  t_out = -1;
         }
-        else
+      else
         {
-            t_out = c_tout*1000;
+	  t_out = c_tout*1000;
         }
 
 
-        if(poll(check ,1 ,t_out) == -1 || poll(check ,1 ,t_out) == -0){
+      // if(poll(check ,1 ,t_out) == -1 || poll(check ,1 ,t_out) == -0){
+      int poller = poll(check, 1, t_out);
+      if(poller == 0){
+	printf("BREAK\n");
+	break;
+      }
 
-	printf("BREAKn");
-            break;
-        }
+      uint8_t ret_data[38]; 
+      printf("trying to recieve\n");
+      int ret = recvfrom(sock, &ret_data, 38, 0, (struct sockaddr *)&server_addr, &len);
+      printf("recieved\n");
 
-        uint8_t ret_data[38]; 
-        printf("trying to recieve\n");
-        int ret = recvfrom(sock, &ret_data, 38, 0, (struct sockaddr *)&server_addr, &len);
-        printf("recieved\n");
-
-        if (ret>= 0) {
+      if (ret>= 0) {
 
         struct timespec curr_t;
         clock_gettime(CLOCK_REALTIME,&curr_t);
@@ -221,42 +227,40 @@ int main(int argc, char *argv[])
         s_nsec = be64toh(s_nsec);
 
         if(finish[seq-1].seq != 0){
-            i--;
+	  i--;
         }
         else
-        {
+	  {
 
-        long double temp = c_nsec/1000000000;
-        long double t0 = c_sec + temp;
-        temp = s_nsec/1000000000;
-        long double t1 = s_sec + temp;
-        temp = nsec_rn/1000000000;
-        long double t2 = sec_rn + temp;
+	    long double temp = c_nsec/1000000000;
+	    long double t0 = c_sec + temp;
+	    temp = s_nsec/1000000000;
+	    long double t1 = s_sec + temp;
+	    temp = nsec_rn/1000000000;
+	    long double t2 = sec_rn + temp;
         
-        float theta = (t1-t0 + t1 - t2)/2;
-        float delta = t2 - t0;
-        finish[seq-1].seq = seq;
-        finish[seq-1].delta = delta;
-        finish[seq-1].theta = theta;
+	    float theta = (t1-t0 + t1 - t2)/2;
+	    float delta = t2 - t0;
+	    finish[seq-1].seq = seq;
+	    finish[seq-1].delta = delta;
+	    finish[seq-1].theta = theta;
 
-        }
-        }
-        else break;
-
-        i++;
-
-
+	  }
+      }
+      else {
+	break;
+      }
+      i++;
     }
 
-	printf("BREAKn");
-     close(sock);
+  close(sock);
 
-    for(int i =0;i < (int)client.treq; i++){
-        if(finish[i].seq == 0)
-            printf("%d: Dropped\n", i+1);
-        else
-            printf("%d: %.4f %.4f\n", i+1, finish[i].theta, finish[i].delta);
-    }
+  for(int i =0;i < (int)client.treq; i++){
+    if(finish[i].seq == 0)
+      printf("%d: Dropped\n", i+1);
+    else
+      printf("%d: %.4f %.4f\n", i+1, finish[i].theta, finish[i].delta);
+  }
 
 
 	
--- assignment1/server.cpp
+++ assignment1/server.cpp
@@ -1,4 +1,3 @@
-
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,15 +13,15 @@
 using std::cout;
 using std::endl;
 struct server_arguments {
-	int port;
-	int drop_perc;
+  int port;
+  int drop_perc;
 };
 
 typedef struct myNode {
-	time_t update;
-    int seq;
-    struct sockaddr_in ip_port;
-    struct myNode *next;
+  time_t update;
+  int seq;
+  struct sockaddr_in ip_port;
+  struct myNode *next;
 }Node;
 
 
@@ -30,274 +29,270 @@ typedef struct myNode {
 
 error_t server_parser(int key, char *arg, struct argp_state *state) {
 
-	struct server_arguments *args = (server_arguments* )(state->input);
-	error_t ret = 0;
-	switch(key) {
-	case 'p':
-		/* Validate that port is correct and a number, etc!! */
-		args->port = atoi(arg);
-		if (args->port == 0 || args->port <= 1024 /* port is invalid */) {
-			argp_error(state, "Invalid option for a port");
-		}
-		break;
-	case 'd':
-        args->drop_perc = atoi(arg);
+  struct server_arguments *args = (server_arguments* )(state->input);
+  error_t ret = 0;
+  switch(key) {
+  case 'p':
+    /* Validate that port is correct and a number, etc!! */
+    args->port = atoi(arg);
+    if (args->port == 0 || args->port <= 1024 /* port is invalid */) {
+      argp_error(state, "Invalid option for a port");
+    }
+    break;
+  case 'd':
+    args->drop_perc = atoi(arg);
 		
-		break;
-	default:
-		ret = ARGP_ERR_UNKNOWN;
-		break;
-	}
+    break;
+  default:
+    ret = ARGP_ERR_UNKNOWN;
+    break;
+  }
     
-	return ret;
+  return ret;
 }
 
 void *server_parseopt(struct server_arguments *args,int argc, char *argv[]) {
 
-	memset(args, 0, sizeof(*args));
+  memset(args, 0, sizeof(*args));
 
 
-	struct argp_option options[] = {
-		{ "port", 'p', "port", 0, "The port to be used for the server" ,0},
-		{ "dropped_perc", 'd', "dropped_perc", 0, "The percentage dropped", 0},
-		{0}
-	};
-	struct argp argp_settings = { options, server_parser, 0, 0, 0, 0, 0 };
-	if (argp_parse(&argp_settings, argc, argv, 0, NULL, args) != 0) {
-		printf("Got an error condition when parsing \n");
-	}
+  struct argp_option options[] = {
+    { "port", 'p', "port", 0, "The port to be used for the server" ,0},
+    { "dropped_perc", 'd', "dropped_perc", 0, "The percentage dropped", 0},
+    {0}
+  };
+  struct argp argp_settings = { options, server_parser, 0, 0, 0, 0, 0 };
+  if (argp_parse(&argp_settings, argc, argv, 0, NULL, args) != 0) {
+    printf("Got an error condition when parsing \n");
+  }
 
-	/* What happens if you don't pass in parameters? Check args values
-	 * for sanity and required parameters being filled in */
+  /* What happens if you don't pass in parameters? Check args values
+   * for sanity and required parameters being filled in */
 
-	if (!args->port) {
-		fputs("A port number must be specified \n", stderr);
-		exit(1);
-	}
+  if (!args->port) {
+    fputs("A port number must be specified \n", stderr);
+    exit(1);
+  }
 
-	/* If they don't pass in all required settings, you should detect
-	 * this and return a non-zero value from main */
-	printf("Got port %d and dropped percentage %d\n", args->port, args->drop_perc);
+  if (!args->drop_perc) {
+    args->drop_perc = 0;
+  }
 
-	return args;
+  /* If they don't pass in all required settings, you should detect
+   * this and return a non-zero value from main */
+  printf("Got port %d and dropped percentage %d\n", args->port, args->drop_perc);
+
+  return args;
 }
 
 Node *update_list(Node *head, sockaddr_in ip_port, int new_seq, time_t time)
 {
 	
-	//if head is null, first addition
+  //if head is null, first addition
 
 
-	Node *curr = head;
-	Node *prev = NULL;
-	int found = 0;
-	if(head == NULL)
-	{
-		Node *newnode = (Node*)malloc(sizeof(Node));
-		memset(newnode,0,sizeof(Node));
+  Node *curr = head;
+  Node *prev = NULL;
+  int found = 0;
+  if(head == NULL)
+    {
+      Node *newnode = (Node*)malloc(sizeof(Node));
+      memset(newnode,0,sizeof(Node));
 
-		newnode->ip_port = ip_port;
-		newnode->seq = new_seq;
-		newnode->update = time;
-		newnode->next = NULL;
+      newnode->ip_port = ip_port;
+      newnode->seq = new_seq;
+      newnode->update = time;
+      newnode->next = NULL;
 
-		return newnode;
-	}
-	else{
+      return newnode;
+    }
+  else{
 	
-	//else you have to traverse through the list
-	//make a current and previous pointer
+    //else you have to traverse through the list
+    //make a current and previous pointer
 
 
-	//while end not reached
+    //while end not reached
 
-	while(curr != NULL)
-    {   
-		//save currentaddr and currentport
-		sockaddr_in curr_ip_port = curr->ip_port;
-		//if you find the same client , found = 1(found)
+    while(curr != NULL)
+      {   
+	//save currentaddr and currentport
+	sockaddr_in curr_ip_port = curr->ip_port;
+	//if you find the same client , found = 1(found)
         if(curr_ip_port.sin_addr.s_addr == ip_port.sin_addr.s_addr && 
-											curr_ip_port.sin_port == ip_port.sin_port){
-            found = 1;
+	   curr_ip_port.sin_port == ip_port.sin_port){
+	  found = 1;
 
-			//if you are past the last update of the same addr update it with new one
-            if(difftime(curr->update, time) >= 120){
+	  //if you are past the last update of the same addr update it with new one
+	  if(difftime(curr->update, time) >= 120){
 
-                curr->update = time;
-                curr->seq = new_seq;
-            }
-			else
-			{
-				//else check if the current list seq > parameter seq 
-                if(curr->seq > new_seq){
-					//print as shown in description
-                    printf("%s:%s %d %d\n", curr->ip_port.sin_addr, curr->ip_port.sin_port, new_seq, curr->seq);
-                }else{
-					// update accordingly
+	    curr->update = time;
+	    curr->seq = new_seq;
+	  }
+	  else
+	    {
+	      //else check if the current list seq > parameter seq 
+	      if(curr->seq > new_seq){
+		//print as shown in description
+		printf("%s:%s %d %d\n", curr->ip_port.sin_addr, curr->ip_port.sin_port, new_seq, curr->seq);
+	      }else{
+		// update accordingly
 
-                    curr->update = time;
-                    curr->seq = new_seq;
-                }
+		curr->update = time;
+		curr->seq = new_seq;
+	      }
             }
         }
-		else{
+	else{
 
-			//if not the same client but greater than 120 , replace
-            if(difftime(time, curr->update) >= 120){
-                curr->update = time;
-                curr->seq = 0;
-            }
+	  //if not the same client but greater than 120 , replace
+	  if(difftime(time, curr->update) >= 120){
+	    curr->update = time;
+	    curr->seq = 0;
+	  }
         }
 
 	
 	//change current and prev
         prev = curr ;
         curr = curr->next;
-    }
+      }
 
-	}
-	//end while
+  }
+  //end while
 
-	if(found==0){
+  if(found==0){
 
-		Node *newnode = (Node*)malloc(sizeof(Node));
-		memset(newnode,0,sizeof(Node));
+    Node *newnode = (Node*)malloc(sizeof(Node));
+    memset(newnode,0,sizeof(Node));
 
-		newnode->ip_port = ip_port;
-		newnode->seq = new_seq;
-		newnode->update = time;
-		newnode->next = NULL;
+    newnode->ip_port = ip_port;
+    newnode->seq = new_seq;
+    newnode->update = time;
+    newnode->next = NULL;
 
-		prev->next = newnode; 
-    }
+    prev->next = newnode; 
+  }
 
-    return head;
+  return head;
 
 }
 
 void rec_data(int new_socket, int drop)
 {
-	Node *client_list = NULL;
+  Node *client_list = NULL;
 
-    while(true){
-        struct sockaddr_in client; // Client address
-        socklen_t len = sizeof(client);
+  while(true){
+    struct sockaddr_in client; // Client address
+    socklen_t len = sizeof(client);
 
-        // Client sends buffer and recieve here 
-        uint8_t data[22]; 
+    // Client sends buffer and recieve here 
+    uint8_t data[22]; 
 
-        recvfrom(new_socket, data, 22, 0, (struct sockaddr *) &client, &len ); 
+    recvfrom(new_socket, &data, 22, 0, (struct sockaddr *) &client, &len ); 
 
-		//check for dropped Percentage 
-		 int random = ((rand()%(100+1)+0));
+    //check for dropped Percentage 
+    int random = ((rand()%(100+1)+0));
         
-        if(drop == 0 || random > drop){
+    if(drop == 0 || random > drop){
 
-		//store current time stamp
-        struct timespec curr_t;
-		clock_gettime(CLOCK_REALTIME,&curr_t);
+      //store current time stamp
+      struct timespec curr_t;
+      clock_gettime(CLOCK_REALTIME,&curr_t);
 
-		time_t sec = curr_t.tv_sec;
-		long nsec = curr_t.tv_nsec;
+      time_t sec = curr_t.tv_sec;
+      long nsec = curr_t.tv_nsec;
 
-		//store what the client sent
-		uint32_t seq_cli;
-		uint64_t sec_cli;
-		uint64_t nsec_cli;
+      //store what the client sent
+      uint32_t seq_cli;
+      uint64_t sec_cli;
+      uint64_t nsec_cli;
         
-		memcpy(&seq_cli, data, 4);
-        memcpy(&sec_cli, data+6, 8);
-		memcpy(&nsec_cli,data+14,  8);
+      memcpy(&seq_cli, data, 4);
+      memcpy(&sec_cli, data+6, 8);
+      memcpy(&nsec_cli,data+14,  8);
 		
-		seq_cli = ntohl(seq_cli); 
-        sec_cli = be64toh(sec_cli);
-        nsec_cli = be64toh(nsec_cli);
+      seq_cli = ntohl(seq_cli); 
+      sec_cli = be64toh(sec_cli);
+      nsec_cli = be64toh(nsec_cli);
 
-		//this function call should change the parameters accordingly 
-		client_list = update_list(client_list, client, seq_cli, sec);
+      //this function call should change the parameters accordingly 
+      client_list = update_list(client_list, client, seq_cli, sec);
 
-		//reconstruct data buffer to send back content
-		uint8_t ret_data[38];
+      //reconstruct data buffer to send back content
+      uint8_t ret_data[38];
 
-		seq_cli = htonl(seq_cli); 
-        sec_cli = htobe64(sec_cli);
-        nsec_cli = htobe64(nsec_cli);
+      seq_cli = htonl(seq_cli); 
+      sec_cli = htobe64(sec_cli);
+      nsec_cli = htobe64(nsec_cli);
 
-		uint16_t ver_cli = 7;
-        ver_cli = htons(ver_cli);
+      uint16_t ver_cli = 7;
+      ver_cli = htons(ver_cli);
 
-		uint64_t serv_sec_nb = htobe64(sec);
-        uint64_t serv_nanosec_nb = htobe64(nsec);
+      uint64_t serv_sec_nb = htobe64(sec);
+      uint64_t serv_nanosec_nb = htobe64(nsec);
 
-		//data to be sent seq , version, seconds, nano seconds,
-		// server neconds, server nanoseconds(8 + 8 + 2 + 4 + 8 + 8)
+      //data to be sent seq , version, seconds, nano seconds,
+      // server neconds, server nanoseconds(8 + 8 + 2 + 4 + 8 + 8)
 
-		memcpy(ret_data, &seq_cli, 4);
-        memcpy(ret_data+4, &ver_cli, 2);
-        memcpy(ret_data+6, &sec_cli, 8);
-        memcpy(ret_data+14, &nsec_cli, 8);
-        memcpy(ret_data+22, &serv_sec_nb, 8);
-        memcpy(ret_data+30, &serv_nanosec_nb, 8);
+      memcpy(ret_data, &seq_cli, 4);
+      memcpy(ret_data+4, &ver_cli, 2);
+      memcpy(ret_data+6, &sec_cli, 8);
+      memcpy(ret_data+14, &nsec_cli, 8);
+      memcpy(ret_data+22, &serv_sec_nb, 8);
+      memcpy(ret_data+30, &serv_nanosec_nb, 8);
 
-		//sent to client
-		printf("while  befote loop check \n");
-		sendto(new_socket, ret_data, 38, 0, (struct sockaddr *) &client.sin_addr, sizeof(client));
-		printf("while loop check \n");
+      //sent to client
+      printf("while  befote loop check \n");
+      sendto(new_socket, ret_data, 38, 0, (struct sockaddr *) &client.sin_addr, sizeof(client));
+      printf("while loop check \n");
 
-	}
+    }
 
-	}
+  }
 
 }
 
 
-void create(struct server_arguments args){
+void create(struct server_arguments args, int servSock){
 
-	// Create socket for incoming connections
-	int servSock, new_socket; // Socket descriptor for server
+  // Construct local address structure
+  struct sockaddr_in servAddr; // Local address
+  int add_len = sizeof(servAddr);
+  memset(&servAddr, 0, sizeof(servAddr)); // Zero out structure
+  servAddr.sin_family = AF_INET; // IPv4 address family
+  servAddr.sin_addr.s_addr = INADDR_ANY; // Any incoming interface
+  servAddr.sin_port = htons(args.port); // Local port
 
-	if ((servSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-		perror("socket() failed");
-		exit(1);
-	}
-
-	// Construct local address structure
-	struct sockaddr_in servAddr; // Local address
-	int add_len = sizeof(servAddr);
-	memset(&servAddr, 0, sizeof(servAddr)); // Zero out structure
-	servAddr.sin_family = AF_INET; // IPv4 address family
-	servAddr.sin_addr.s_addr = htonl(INADDR_ANY); // Any incoming interface
-	servAddr.sin_port = htons(args.port); // Local port
-
-	// Bind to the local address
-	if (bind(servSock, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0) {
-		perror("bind() failed");
-		exit(1);
-	}
+  // Bind to the local address
+  if (bind(servSock, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0) {
+    perror("bind() failed");
+    exit(1);
+  }
 
 
 
-	// Mark the socket so it will listen for incoming connections
-	if (listen(servSock, 1) < 0) {
-		perror("listen() failed");
-		exit(1);
-	}
-	else{
-			cout<<"bollo ja bolbe bolo"<<endl;
-	}
+  // Mark the socket so it will listen for incoming connections
+  // if (listen(servSock, 1) < 0) {
+  // 	perror("listen() failed");
+  // 	exit(1);
+  // }
+  else{
+    cout<<"bollo ja bolbe bolo"<<endl;
+  }
 	
 
 
 
 	
-	if ((new_socket = accept(servSock, (struct sockaddr *)&servAddr,  
-                       (socklen_t*)&add_len))<0) 
-    { 
-        perror("accept"); 
-        exit(EXIT_FAILURE); 
-    } 
+  // 	if ((new_socket = accept(servSock, (struct sockaddr *)&servAddr,  
+  //                    (socklen_t*)&add_len))<0) 
+  // { 
+  //     perror("accept"); 
+  //     exit(EXIT_FAILURE); 
+  // } 
 
-	rec_data(new_socket,args.drop_perc);
+  rec_data(servSock, args.drop_perc);
 
 	
 }
@@ -306,11 +301,16 @@ void create(struct server_arguments args){
 
 
 int main(int argc, char *argv[]){
+  int servSock;
+  if ((servSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+    perror("socket() failed");
+    exit(1);
+  }
 	
-	struct server_arguments args;
-	server_parseopt(&args, argc, argv); 
-	create(args);
+  struct server_arguments args;
+  server_parseopt(&args, argc, argv); 
+  create(args, servSock);
 
 	
-	return 1;
+  return 1;
 }
